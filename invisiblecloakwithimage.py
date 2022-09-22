0219460b7302f3229202271b3a51ef85483e670f
SP248 <82733585+SP248@users.noreply.github.com>
2022-09-21 21:09:46 -0400

    Add files via upload

---
+++ invisiblecloakwithimage.py
@@ -0,0 +1,28 @@
+from os import access
+import cv2
+import time
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+image = cv2.imread("39632.webp")
+
+while(True):
+    ret,frame=cap.read()
+    print(frame)
+    frame = cv2.resize(frame, (640,480))
+    image = cv2.resize(image, (640,480))
+    
+    u_black = np.array([104,153,70])
+    l_black=np.array([30,30,0])
+    mask=cv2.inRange(frame,l_black,u_black)
+    res=cv2.bitwise_and(frame,frame,mask = mask)
+    f = frame-res
+    f = np.where(f ==0,image,f)
+    cv2.imshow("video",frame)
+    cv2.imshow("mask",f)
+    if cv2.waitKey(1)&0xFF==ord("q"):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
